eff4433ddbd958910b836756e934c8de4af738cb
Mix <O0mix0O>
2023-06-24 17:42:18 +0100

    tidy u[

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import './App.css';
-import CalculatorInterface from './CalculatorInterface';
+import "./App.css";
+import CalculatorInterface from "./CalculatorInterface";
 
 function App() {
-  const handleAddCalculator = (calculator) => {
-    // Add the logic to handle the calculator addition here
-    console.log(`Adding calculator: ${calculator}`);
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-      <CalculatorInterface onAddCalculator={handleAddCalculator} />
-    </header>
+        <CalculatorInterface />
+      </header>
     </div>
   );
 }
